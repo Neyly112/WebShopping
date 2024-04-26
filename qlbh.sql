cd998eddd166835951fa96825ba156775d3255e0
embemay77 <162908000+embemay77@users.noreply.github.com>
2024-04-26 18:20:26 +0700

    upload data
    upload data

--- qlbh.sql
+++ qlbh.sql
@@ -1,57 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th4 26, 2024 lúc 12:34 PM
--- Phiên bản máy phục vụ: 10.4.32-MariaDB
--- Phiên bản PHP: 8.0.30
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Cơ sở dữ liệu: `qlbh`
---
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `donhang`
---
-
-CREATE TABLE `donhang` (
-  `MaDonHang` int(11) NOT NULL,
-  `NgayTao` date DEFAULT NULL,
-  `PhuongThucThanhToan` varchar(255) DEFAULT NULL,
-  `MaNguoiDung` int(11) DEFAULT NULL,
-  `HoTen` varchar(255) DEFAULT NULL,
-  `SoDienThoai` varchar(255) DEFAULT NULL,
-  `DiaChiGiaoHang` varchar(255) DEFAULT NULL,
-  `TrangThai` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `loai`
---
-
 CREATE TABLE `loai` (
   `MaLoai` int(11) NOT NULL,
   `TenLoai` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Đang đổ dữ liệu cho bảng `loai`
---
 
 INSERT INTO `loai` (`MaLoai`, `TenLoai`) VALUES
 (1, 'THỜI TRANG NỮ'),
@@ -60,27 +18,17 @@ INSERT INTO `loai` (`MaLoai`, `TenLoai`) VALUES
 (4, 'OLD COLLECTION'),
 (11, 'BEST SELLER');
 
--- --------------------------------------------------------
 
---
--- Cấu trúc bảng cho bảng `nguoidung`
---
-
-CREATE TABLE `nguoidung` (
-  `MaNguoiDung` int(11) NOT NULL,
-  `TenNguoiDung` varchar(255) DEFAULT NULL,
+CREATE TABLE `donhang` (
+  `MaDonHang` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `NgayTao` date DEFAULT CURRENT_TIMESTAMP,
+  `PhuongThucThanhToan` varchar(255) DEFAULT 'Thanh toán khi nhận hàng',
+  `HoTen` varchar(255) DEFAULT NULL,
   `SoDienThoai` varchar(255) DEFAULT NULL,
-  `Email` varchar(255) DEFAULT NULL,
-  `DiaChi` varchar(255) DEFAULT NULL,
-  `MatKhau` varchar(255) DEFAULT NULL,
-  `VaiTro` varchar(255) DEFAULT NULL COMMENT 'admin, user'
+  `DiaChiGiaoHang` varchar(255) DEFAULT NULL,
+  `TrangThai` varchar(255) DEFAULT 'Vừa tạo đơn'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `sanpham`
---
 
 CREATE TABLE `sanpham` (
   `MaSanPham` int(11) NOT NULL,
@@ -91,101 +39,43 @@ CREATE TABLE `sanpham` (
   `GiaBan` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Đang đổ dữ liệu cho bảng `sanpham`
---
 
 INSERT INTO `sanpham` (`MaSanPham`, `MaLoai`, `TenSanPham`, `HinhAnh`, `MoTa`, `GiaBan`) VALUES
 (15, 11, 'ao', '1714097864.jpg', 'efaf', '50000'),
 (17, 11, 'aoo', '1714098206.jpg', 'faas', '10000');
 
--- --------------------------------------------------------
 
---
--- Cấu trúc bảng cho bảng `sanphamgiohang`
---
 
 CREATE TABLE `sanphamgiohang` (
   `MaSanPham` int(11) DEFAULT NULL,
   `SoLuongMua` int(11) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Đang đổ dữ liệu cho bảng `sanphamgiohang`
---
+
 
 INSERT INTO `sanphamgiohang` (`MaSanPham`, `SoLuongMua`) VALUES
 (17, 1);
 
---
--- Chỉ mục cho các bảng đã đổ
---
 
---
--- Chỉ mục cho bảng `donhang`
---
-ALTER TABLE `donhang`
-  ADD PRIMARY KEY (`MaDonHang`),
-  ADD KEY `MaNguoiDung` (`MaNguoiDung`);
-
---
--- Chỉ mục cho bảng `loai`
---
 ALTER TABLE `loai`
   ADD PRIMARY KEY (`MaLoai`);
 
---
--- Chỉ mục cho bảng `nguoidung`
---
-ALTER TABLE `nguoidung`
-  ADD PRIMARY KEY (`MaNguoiDung`);
-
---
--- Chỉ mục cho bảng `sanpham`
---
 ALTER TABLE `sanpham`
   ADD PRIMARY KEY (`MaSanPham`),
   ADD KEY `MaLoai` (`MaLoai`);
 
---
--- Chỉ mục cho bảng `sanphamgiohang`
---
+
 ALTER TABLE `sanphamgiohang`
   ADD KEY `MaSanPham` (`MaSanPham`);
 
---
--- AUTO_INCREMENT cho các bảng đã đổ
---
-
---
--- AUTO_INCREMENT cho bảng `sanpham`
---
 ALTER TABLE `sanpham`
   MODIFY `MaSanPham` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
---
--- Các ràng buộc cho các bảng đã đổ
---
-
---
--- Các ràng buộc cho bảng `donhang`
---
-ALTER TABLE `donhang`
-  ADD CONSTRAINT `donhang_ibfk_1` FOREIGN KEY (`MaNguoiDung`) REFERENCES `nguoidung` (`MaNguoiDung`);
-
---
--- Các ràng buộc cho bảng `sanpham`
---
 ALTER TABLE `sanpham`
   ADD CONSTRAINT `sanpham_ibfk_1` FOREIGN KEY (`MaLoai`) REFERENCES `loai` (`MaLoai`);
 
---
--- Các ràng buộc cho bảng `sanphamgiohang`
---
+
 ALTER TABLE `sanphamgiohang`
   ADD CONSTRAINT `sanphamgiohang_ibfk_1` FOREIGN KEY (`MaSanPham`) REFERENCES `sanpham` (`MaSanPham`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
