f5e681be709fff50dfcde7c5b9e36b84c9e1315d
embemay77 <162908000+embemay77@users.noreply.github.com>
2024-04-26 17:35:13 +0700

    Update qlbh.sql

--- qlbh.sql
+++ qlbh.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th4 22, 2024 lúc 05:31 PM
--- Phiên bản máy phục vụ: 10.4.28-MariaDB
--- Phiên bản PHP: 8.0.28
+-- Thời gian đã tạo: Th4 26, 2024 lúc 12:34 PM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,23 +37,6 @@ CREATE TABLE `donhang` (
   `DiaChiGiaoHang` varchar(255) DEFAULT NULL,
   `TrangThai` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `dsyeuthich`
---
-
-CREATE TABLE `dsyeuthich` (
-  `MaSanPham` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Đang đổ dữ liệu cho bảng `dsyeuthich`
---
-
-INSERT INTO `dsyeuthich` (`MaSanPham`) VALUES
-(1);
 
 -- --------------------------------------------------------
 
@@ -113,20 +96,8 @@ CREATE TABLE `sanpham` (
 --
 
 INSERT INTO `sanpham` (`MaSanPham`, `MaLoai`, `TenSanPham`, `HinhAnh`, `MoTa`, `GiaBan`) VALUES
-(1, 11, 'ÁO', '__-12_7532a4063c714c05bc5108246f8d0e98_1024x1024.webp', 'ao', '10000'),
-(2, 11, 'QUẦN', '__-12_ee33ed7597524ab19fbe40c0d21493f8_1024x1024.jpg', 'ao', '10000'),
-(3, 11, 'taaa', 'main-02_34f02cc8f263402bab305aa9e1a35ce1_1024x1024.webp', 'ao', '1000'),
-(4, 11, 'áo', '__-12_ee33ed7597524ab19fbe40c0d21493f8_1024x1024.jpg', 'ao', '1000'),
-(5, 11, '11', 'main-02_34f02cc8f263402bab305aa9e1a35ce1_1024x1024.webp', 'ao', '12323'),
-(6, 11, '1', 'IMG_6526_result.webp', 'ao', '2'),
-(7, 11, '2232', 'NAAU8636.jpg', 'ao', '123'),
-(8, 11, 'tr', 'Apparel and Accessories TikTok In-Feed Ad in White Grey Minimalist Monotone Style Mobile Video.jpg', 'ao', 'asd'),
-(9, 11, 'asd', 'Apparel and Accessories TikTok In-Feed Ad in White Grey Minimalist Monotone Style Mobile Video.jpg', 'ao', 'sda'),
-(10, 11, 'qweew', 'Ecru White Modern Elegant Minimal Women_s Fashion Clothing Collection Instagram Story.png', 'ao', 'we2'),
-(11, 11, '10', '__-12_ee33ed7597524ab19fbe40c0d21493f8_1024x1024.jpg', 'ao', '2eq'),
-(12, 11, 'sas', 'Ecru White Modern Elegant Minimal Women_s Fashion Clothing Collection Instagram Story.png', 'ao', 's2'),
-(13, 11, '223', 'Apparel and Accessories TikTok In-Feed Ad in White Grey Minimalist Monotone Style Mobile Video.jpg', 'ao', '123'),
-(14, 11, 'dad', 'Apparel and Accessories TikTok In-Feed Ad in White Grey Minimalist Monotone Style Mobile Video (1).jpg', 'ao', '2312');
+(15, 11, 'ao', '1714097864.jpg', 'efaf', '50000'),
+(17, 11, 'aoo', '1714098206.jpg', 'faas', '10000');
 
 -- --------------------------------------------------------
 
@@ -135,10 +106,16 @@ INSERT INTO `sanpham` (`MaSanPham`, `MaLoai`, `TenSanPham`, `HinhAnh`, `MoTa`, `
 --
 
 CREATE TABLE `sanphamgiohang` (
-  `MaGioHang` int(11) NOT NULL,
   `MaSanPham` int(11) DEFAULT NULL,
-  `SoLuongMua` int(11) DEFAULT NULL
+  `SoLuongMua` int(11) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `sanphamgiohang`
+--
+
+INSERT INTO `sanphamgiohang` (`MaSanPham`, `SoLuongMua`) VALUES
+(17, 1);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -150,12 +127,6 @@ CREATE TABLE `sanphamgiohang` (
 ALTER TABLE `donhang`
   ADD PRIMARY KEY (`MaDonHang`),
   ADD KEY `MaNguoiDung` (`MaNguoiDung`);
-
---
--- Chỉ mục cho bảng `dsyeuthich`
---
-ALTER TABLE `dsyeuthich`
-  ADD PRIMARY KEY (`MaSanPham`);
 
 --
 -- Chỉ mục cho bảng `loai`
@@ -180,7 +151,6 @@ ALTER TABLE `sanpham`
 -- Chỉ mục cho bảng `sanphamgiohang`
 --
 ALTER TABLE `sanphamgiohang`
-  ADD PRIMARY KEY (`MaGioHang`),
   ADD KEY `MaSanPham` (`MaSanPham`);
 
 --
@@ -191,7 +161,7 @@ ALTER TABLE `sanphamgiohang`
 -- AUTO_INCREMENT cho bảng `sanpham`
 --
 ALTER TABLE `sanpham`
-  MODIFY `MaSanPham` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `MaSanPham` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
@@ -202,12 +172,6 @@ ALTER TABLE `sanpham`
 --
 ALTER TABLE `donhang`
   ADD CONSTRAINT `donhang_ibfk_1` FOREIGN KEY (`MaNguoiDung`) REFERENCES `nguoidung` (`MaNguoiDung`);
-
---
--- Các ràng buộc cho bảng `dsyeuthich`
---
-ALTER TABLE `dsyeuthich`
-  ADD CONSTRAINT `dsyeuthich_ibfk_1` FOREIGN KEY (`MaSanPham`) REFERENCES `sanpham` (`MaSanPham`);
 
 --
 -- Các ràng buộc cho bảng `sanpham`
