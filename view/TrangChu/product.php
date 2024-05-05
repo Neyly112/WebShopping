cf3f921a3b6609ca6a5b052c37f34ac49214faa4
Neyly112 <166828743+Neyly112@users.noreply.github.com>
2024-05-05 13:42:37 +0700

    Update product.php

--- view/TrangChu/product.php
+++ view/TrangChu/product.php
@@ -1,49 +1,33 @@
 <?php
-echo "<div class='row container justify-center'>";
-echo "<div class='col-md-6' id='product-img'>";
+
 $imageDirectory = "./view/Uploads/" . $image;
-echo "<img src='{$imageDirectory}' style='width:60%;' />";
-echo "</div>";
-echo "<div class='col-md-4'>";
 
-echo "<div class='product-detail'>Giá Bán:</div>";
-echo "<h4 class='m-b-10px price-description'>" . $giaBan . "</h4>";
-
-echo "<div class='product-detail'>Product description:</div>";
-
-echo "<div class='m-b-10px'>";
-// make html
 $page_description = htmlspecialchars_decode(htmlspecialchars_decode($moTa));
 
-// show to user
-echo $page_description;
-echo "</div>";
-echo "</div>";
-// echo "<div class='product-detail'>Product category:</div>";
-// echo "<div class='m-b-10px'>{$product->category_name}</div>";
-echo "<div class='col-md-2'>";
+?>
 
-// if product was already added in the cart
+<div class='row container justify-center'>
+    <div class='col-md-6' id='product-img'>
+        <img src='<?php echo $imageDirectory; ?>' style='width:80%;' />
+    </div>
+    <div class='col-md-4'>
+        <div class='product-detail'>Giá Bán: </div>
+        <h4 style='color: red;' class='m-b-10px price-description'><?php echo $giaBan; ?></h4>
+        <div class='product-detail'>Mô tả:</div>
+        <div class='m-b-10px'>
+            <?php echo $page_description; ?>
+        </div>
+    </div>
+    <div class='col-md-2'>
+    <form class='add-to-cart-form' onsubmit="showSuccessMessage()">
+        <button style='width:100%; margin-bottom: 10px;' type='submit' class='btn btn-primary add-to-cart'>
+            <a href="index.php?act=addcart&MaSanPham=<?php echo $_GET['MaSanPham']; ?>" style="color: white; text-decoration: none;">Thêm vào giỏ hàng</a>
+        </button>
+        <button style='width:100%;' type='submit' class='btn btn-success'>
+            <a href="index.php?act=muachitietsp&MaSanPham=<?php echo $_GET['MaSanPham']; ?>" style="color: white; text-decoration: none;">Mua hàng</a>
+        </button>
+    </form>
+</div>
 
-
-// if product was not added to the cart yet
-
-
-echo "<form class='add-to-cart-form'>";
-// product id
-
-
-echo "<div class='m-b-10px f-w-b'>Quantity:</div>";
-echo "<input type='number' value='1' class='form-control m-b-10px cart-quantity' min='1' /><br>";
-
-// enable add to cart button
-echo "<button style='width:100%;' type='submit' class='btn btn-primary add-to-cart m-b-10px'>";
-echo "<span class='glyphicon glyphicon-shopping-cart mb-3'></span> Add to cart";
-echo "</button>";
-
-echo "</form>";
-
-echo "</div>";
-
-echo "</div>";
-echo "</div>";
+</div>
+<script src="./view/TrangChu/script.js"></script>
